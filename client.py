27089d618d2d67f8668710b825641c6fd9fb1a36
Gustavo Figueiredo <gustavopfp.trab@gmail.com>
2022-11-23 02:53:44 -0300

    chat room working

--- client.py
+++ client.py
@@ -1,9 +1,8 @@
 import socket
-import select
-import errno
+import threading
 
 # Define the length of the message 
-HEADER = 64
+HEADER = 1024
 # Define the format of the message 
 FORMAT = 'utf-8'
 # Define the port we're runing the server
@@ -15,32 +14,58 @@ ADDR = (SERVER, PORT)
 # Disconectiong message to notify the server
 DISCONECT_MESSAGE = ">DISCONECT<"
 
+# Client username
+
+username = input("Choose a username: ")
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connecting to the server
 client.connect(ADDR)
 
+# def send_msg(msg):
+#     """ Send messages to the server
 
-def send(msg):
-    """ Send messages to the server
-
-    Args:
-        msg (str): message to the server
-    """
-    message = msg.encode(FORMAT) # Encode the string into byte object
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT) # Encode msg_length into string
+#     Args:
+#         msg (str): message to the server
+#     """
+#     message = msg.encode(FORMAT) # Encode the string into byte object
+#     msg_length = len(message)
+#     send_length = str(msg_length).encode(FORMAT) # Encode msg_length into string
     
-    # Pad to make this length 64 
-    send_length += b' ' * (HEADER - len(send_length)) # Get the length of the message and subtract from the header
-    client.send(send_length)
-    client.send(message)
-    print(client.recv(HEADER).decode(FORMAT))
+#     # Pad to make this length 64 
+#     send_length += b' ' * (HEADER - len(send_length)) # Get the length of the message and subtract from the header
+#     client.send(send_length)
+#     client.send(message)
+#     print(client.recv(HEADER).decode(FORMAT))
 
-if __name__ == '__main__': 
+def client_recive():
     while True:
-        user_msg = input()
-        if user_msg is DISCONECT_MESSAGE:
-            
-            False
-        send(user_msg)
+        try:
+            message = client.recv(HEADER).decode(FORMAT)
+            if message == "Username?":
+                # client.send_msg(message)
+                client.send(username.encode(FORMAT))
+            else:
+                print(message) 
+        except:
+            print('[->ERROR<-]')
+            client.close()
+            break
+
+def client_send():
+    while True:
+        user_msg = f"{username}: {input('...')}"
+        client.send(user_msg.encode(FORMAT))
+
+       
+        #client.send(user_msg.encode(FORMAT))
+
+
+
+recive_theread = threading.Thread(target=client_recive)
+recive_theread.start()
+
+send_theread = threading.Thread(target=client_send)
+send_theread.start()
+
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import threading # to make sure that we're not blocking other clients
 
 
 # Define the length of the message 
-HEADER = 64
+HEADER = 1024
 
 # Define the format of the message 
 FORMAT = 'utf-8'
@@ -26,6 +26,25 @@ local_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Define the ki
 # Bound the adress to the socket "local_server"
 local_server.bind(ADDR) 
 
+# List of all the messages sended
+msgs_stored = []
+
+# list of clients
+
+clients = []
+ # clients usernames
+usernames = []
+
+print("[STARTING] your server is starting...")
+print(f"[LISTENING] Server is listening on {SERVER}.")
+local_server.listen() # Listening new conections
+
+
+def broadcast(msg):
+    """Send a message to all the users
+    """
+    for client in clients:
+        client.send(msg)
 
 
 
@@ -39,17 +58,30 @@ def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connect to the server.")
 
     connected = True
-    while connected:
-        msg_length = conn.recv(HEADER) # Define how many bites we will recive from the client
-        if msg_length: # checking if the message isn't null or none
-            msg_length = int(msg_length.decode(FORMAT)) # Decode from byte to UTF format
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONECT_MESSAGE:
-                connected = False
-            print(f"[{addr}] send: {msg}")
+    while True:
+        try:
+            msg_length = conn.recv(HEADER) # Define how many bites we will recive from the client
+            broadcast(msg) # send the message for everyone in the room
+            if msg_length: # checking if the message isn't null or none
+                msg_length = int(msg_length.decode(FORMAT)) # Decode from byte to UTF format
+                msg = conn.recv(msg_length).decode(FORMAT)
+                if msg == DISCONECT_MESSAGE:
+                    connected = False
+                print(f"[{addr}] send: {msg}")
 
-            # Send a message to the client
-            conn.send("[MESSAGE RECIVED] ...".encode(FORMAT))
+                # Send a message to the client
+                conn.send("[MESSAGE RECIVED] ...".encode(FORMAT))
+        except:
+            index = clients.index(conn)
+            clients.remove(conn)
+            conn.close()
+            username = usernames[index]
+            broadcast(f"[CHECKOUT] {username} has left the chat room.".encode(FORMAT))
+            usernames.remove(username)
+            connected = False
+            
+            
+               
     
     conn.close()
 
@@ -62,19 +94,28 @@ def start(server):
     Args:
         server (_type_): server ip
     """
-    print("[STARTING] your server is starting...")
-    print(f'[LISTENING] Server is listening on {SERVER}')
     
-    server.listen() # Listening new conections
-
     while True: # The server will keep runing until the server is stopped or crashes
 
        # When a connection occurs the addres of the connection (and a object to send information to thant connection) are stored
        conn,  addr = server.accept()
+       conn.send("Username?".encode(FORMAT))
+       username = conn.recv(HEADER)
+       usernames.append(username)
+       clients.append(conn)
+       print(f"[REGISTERED] The username of this user is: {username}.".encode(FORMAT))
+       broadcast(f"[CHECKIN] {username.decode(FORMAT)} has connected to the chat room.".encode(FORMAT))
+       conn.send(f"[CHECKIN] You are connected {username.decode(FORMAT)}!".encode(FORMAT))
+
+
        thread = threading.Thread(target=handle_client, args=(conn, addr))
        thread.start()
 
        # Print the amount of active connections in ther server
        print(f"[ACTIVE CONNECTIONS] {threading.active_count() -1}") # -1 because the listen connection doesn't count
 
-start(local_server)
+
+
+if __name__ == "__main__":
+    
+    start(local_server)
